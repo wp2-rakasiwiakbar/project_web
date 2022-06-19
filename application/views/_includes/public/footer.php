2c9a4d5d79efe2f9780a5b1995a4670bbbf4a1d9
TBadi <tubagusadi21@gmail.com>
2022-06-19 12:50:25 +0700

    Update footer.php

--- application/views/_includes/public/footer.php
+++ application/views/_includes/public/footer.php
@@ -4,7 +4,7 @@
         <div class="row text-center align-items-center flex-row-reverse">
             <div class="col-lg-auto ml-lg-auto">
                 <ul class="list-inline list-inline-dots mb-0">
-                    <li class="list-inline-item"><a href="" target="_blank" class="link-secondary">Alam Koding</a></li>
+                    <li class="list-inline-item"><a href="" target="_blank" class="link-secondary">Kelompok 5</a></li>
                 </ul>
             </div>
             <div class="col-12 col-lg-auto mt-3 mt-lg-0">
